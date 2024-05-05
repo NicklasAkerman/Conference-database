1cb57875b45860afa131b045dbe8a9273b36ffaa
nicklas <nicklas.akerman@myy.haaga-helia.fi>
2024-05-05 15:17:20 +0300

    views

--- CREATE_INDEX_microsoft.sql
+++ CREATE_INDEX_microsoft.sql
@@ -1,2 +1,4 @@
 CREATE INDEX idx_konf_nimi ON Konferenssi (konf_nimi, id);
 CREATE INDEX idx_hlo_nimi ON Henkilo (sukunimi, etunimi, id);
+CREATE INDEX idx_konf_ilmo_id ON Konf_ilmoittautuminen (konferenssi_id, id);
+
--- CREATE_VIEW_microsoft.sql
+++ CREATE_VIEW_microsoft.sql
@@ -3,4 +3,25 @@ CREATE VIEW Ilmoittautuneiden_maara AS
 SELECT K.konf_nimi, count (*)  'Ilmoittautujamäärä' 
 FROM Konf_ilmoittautuminen KI JOIN konferenssi K ON K.id = KI.konferenssi_id 
 GROUP BY konf_nimi;
-SELECT * FROM Ilmoittautuneiden_maara
+SELECT * FROM Ilmoittautuneiden_maara;
+
+-- ILMOTTAUNEIDEN MÄÄRÄ KONFERENSSIN NIMEN PERUSTEELLA HAKIEN
+CREATE VIEW Ilmoittautuneiden_maara_nimella AS 
+SELECT K.konf_nimi, count (*)  'Ilmoittautujamäärä' 
+FROM Konf_ilmoittautuminen KI JOIN konferenssi K ON K.id = KI.konferenssi_id 
+where konf_nimi LIKE 'Sijoita %'
+GROUP BY konf_nimi;
+SELECT * FROM Ilmoittautuneiden_maara_nimella;
+
+
+-- ESITELMÄT TEEMOITTAIN
+Konferenssitaulun konf_nimi, teema taulun teeman nimi, esitelmä taulusta esitelmän aihe
+
+
+-- KONFERENSSI OHJELMA
+
+-- PROCEEDEINGS
+
+--VAHVISTUSKIRJEET ILMOITTAUTUMISESTA
+
+-- OSALLISTUJALISTAN KOONTI
